782cfffd302ed9533ab022e2c8be49fa623ada3f
artemshakun <artem.shakun.91@gmail.com>
2023-02-18 13:46:30 +0200

    update-app

--- countries/src/pages/Details.jsx
+++ countries/src/pages/Details.jsx
@@ -1,37 +1,22 @@
 import { useNavigate, useParams } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
 import { IoArrowBack } from 'react-icons/io5';
 
 import { Button } from '../components/Button';
 import { Info } from '../components/Info';
-import { selectDetails } from '../store/details/details-selector';
-import {
-    clearDetails,
-    loadCountryByName,
-} from '../store/details/details-actions';
+
 
 export const Details = () => {
-    const { name } = useParams();
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const { currentCountry, error, status } = useSelector(selectDetails);
+  const { name } = useParams();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        dispatch(loadCountryByName(name));
-        return () => {
-            dispatch(clearDetails());
-        };
-    }, [name, dispatch]);
-    console.log(status);
-    return (
-        <div>
-            <Button onClick={() => navigate(-1)}>
-                <IoArrowBack /> Back
-            </Button>
-            {status === 'loading' && <h2>Loading...</h2>}
-            {error && <h2>Can't fetch data</h2>}
-            {currentCountry && <Info push={navigate} {...currentCountry} />}
-        </div>
-    );
+  const currentCountry = null;
+
+  return (
+    <div>
+      <Button onClick={() => navigate(-1)}>
+        <IoArrowBack /> Back
+      </Button>
+      {currentCountry && <Info push={navigate} {...currentCountry} />}
+    </div>
+  );
 };
