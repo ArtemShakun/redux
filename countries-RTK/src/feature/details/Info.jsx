a2743de6c25ba6ddcb8258b0f50361900f16ef73
artemshakun <artem.shakun.91@gmail.com>
2023-06-15 14:03:14 +0300

    done

--- countries-RTK/src/feature/details/Info.jsx
+++ countries-RTK/src/feature/details/Info.jsx
@@ -1,8 +1,5 @@
 import styled from 'styled-components';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectNeighbors } from '../store/details/details-selectors';
-import { useEffect } from 'react';
-import { loadNeighborsByBorder } from '../store/details/details-actions';
+import { useNeighbors } from './use-neighbors';
 
 const Wrapper = styled.section`
     margin-top: 3rem;
@@ -106,15 +103,8 @@ export const Info = (props) => {
         push,
     } = props;
 
-    const dispatch = useDispatch();
-    const neighbors = useSelector(selectNeighbors);
-
-    useEffect(() => {
-        if (borders.length) {
-            dispatch(loadNeighborsByBorder(borders));
-        }
-    }, [borders]);
-
+    const neighbors = useNeighbors(borders);
+    console.log(neighbors);
     return (
         <Wrapper>
             <InfoImage src={flag} alt={name} />
---
+++ countries-RTK/src/feature/theme/use-theme.js
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { setTheme } from './theme-slice';
+
+export const useTheme = () => {
+    const theme = useSelector((state) => state.theme);
+    const dispatch = useDispatch();
+
+    const toggleTheme = () =>
+        dispatch(setTheme(theme === 'light' ? 'dark' : 'light'));
+
+    useEffect(() => {
+        document.body.setAttribute('data-theme', theme);
+    }, [theme]);
+
+    return [theme, toggleTheme];
+};
